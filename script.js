3c3743a68559bd76afbffe6712324ca24bcaeefe
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-13 23:18:15 -0500

    html updates , JS changes
    added ID's to buttons for easier selection in JS.
    
    added function to generate quote on page load
    
    added Event Listener to trigger new quote and to trigger "tweet page loading"
    
    added event listener to trigger new quote option

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const api_url = "https://api.quotable.io/random";
 const quote= document.getElementById('quote');
 const author= document.getElementById('author');
 
+// function to get the quote from API
 async function getQuote(url) {
     const response = await fetch(url);
     let data = await response.json();
@@ -12,4 +13,23 @@ async function getQuote(url) {
     author.innerHTML = data.author;
 }
 
-getQuote(api_url);
+// on button click run the function
+document.getElementById('newQuote').addEventListener('click', () => {
+    getQuote(api_url);
+}); 
+
+// tweet function
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " ---- By " + 
+    author.innerHTML, "Tweet Window", "width=600, height=300");
+}
+
+// on button click run the function - tweet
+document.getElementById('twitter').addEventListener('click', () => {
+    tweet();
+});
+
+//  on page load run the function
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote(api_url);
+});
