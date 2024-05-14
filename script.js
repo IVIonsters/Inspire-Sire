fdfc56bd16f541347f33049230d38e2596c759f8
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-13 23:03:59 -0500

    html / node link / JS changes
    added ID's to render quotes to
    
    added package-lock.json for node check in terminal
    
    defined variables for use in async function
    
    rendered quote to pages using JS inner html

---
+++ script.js
@@ -0,0 +1,15 @@
+// api url
+const api_url = "https://api.quotable.io/random";
+
+const quote= document.getElementById('quote');
+const author= document.getElementById('author');
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+
+getQuote(api_url);
